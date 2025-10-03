b8b4da9374319fa4530069923531fac415856ff6
Yariel Zamora del Cueto <yariel@Yariel.yarielzc.pages.dev>
2025-10-03 16:09:44 -0400

    Connecting to mongo atlas

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ class Settings(BaseSettings):
     ALGORITHM_CRYPT: str
     SECRET_TOKEN_KEY: str
     ACCESS_TOKEN_DURATION: int
+    URL_DB: str
 
     model_config = SettingsConfigDict(env_file=".env")
--- db/client.py
+++ db/client.py
@@ -1,3 +1,14 @@
 from pymongo import MongoClient
+from config import Settings
+from functools import lru_cache
 
-db_client = MongoClient().local
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+# LOCAL DEVELOPMENT
+#db_client = MongoClient().local
+
+#PRODUCTION
+db_client = MongoClient(get_settings().URL_DB).ylink
